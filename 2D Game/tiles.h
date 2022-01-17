18f63420845bbb7edcc080ebf3869dd44aa22b2f
kjbenavides <bx_add3@yahoo.com>
2022-01-16 18:03:29 -0800

    Adding documentation to tiles.h

--- 2D Game/tiles.h
+++ 2D Game/tiles.h
@@ -1,22 +1,79 @@
 #ifndef TILES_H_INCLUDED
 #define TILES_H_INCLUDED
 
+/*
+	The Tile Class
+	Tile objects are used for building the tile map,
+	also for collision detection and handling
+*/
 class Tile
 {
 
 public:
-    Tile();
+    Tile(); // Default constructor, doesn't really do anything
+
+	/*
+		Custom constructor to create the tile 
+		@param int x, y tile coordinates
+		@param int w, h tile width and height
+		@param int tileType
+	*/
     Tile(int, int, int, int, int);
-    SDL_Rect rect;
+
+	/*
+		Sets a specific tile at a location
+		Can be used to change the coordinates, size, and type
+		@param int x, y tile coordinates
+		@param int w, h tile width and height
+		@param int tileType
+	*/
     void setTile(int, int, int, int, int);
+
+	/*
+		Changes the tile type
+		@param int tileType
+	*/
     void setTileType(int);
-    void getCoords();
+
+    /*
+		Output the coordinates of the tile to the console
+	*/
+	void getCoords();
+
+	/*
+		Output the width and height of the tile to the console
+	*/
     void getDim();
+
+	/*
+		@returns an int, tileType
+	*/
     int getTileType();
+
+	/*
+		@returns an int, the tile's starting X coordinate
+	*/
     int getInitialX();
+
+	/*
+		@returns an int, the tile's starting Y coordinate
+	*/
     int getInitialY();
-    void setInitialY(int);
+
+	/*
+		Sets the tile's starting X coordinate
+		Useful for movable tiles
+		@param int velX, for determining where to set X
+	*/
     void setInitialX(int);
+
+	/*
+		Sets the tile's starting Y coordinate
+		@param int velY, for determining where to set Y
+	*/
+    void setInitialY(int);
+
+    SDL_Rect rect;
     int tileType;
 private:
     int initialX;
@@ -27,6 +84,7 @@ private:
     int h;
 };
 
+// Tile types for use by other modules
 extern int Wall;
 extern int Floor;
 extern int Box;
@@ -34,6 +92,11 @@ extern int Box;
 extern Tile tile[];
 extern SDL_Rect renderTileRect;
 
+/*
+	Reads the game's map file, "map"
+	Creates the map from the file
+	@returns
+*/
 bool loadMap();
 
 #endif // TILES_H_INCLUDED
