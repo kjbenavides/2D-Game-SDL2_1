7d984fe1d6d5acb05a626e74a6279e3730e4aa64
kjbenavides <45186333+kjbenavides@users.noreply.github.com>
2022-01-17 01:38:12 -0800

    Update globals.h

--- 2D Game/globals.h
+++ 2D Game/globals.h
@@ -1,6 +1,10 @@
 #ifndef GLOBALS_H_INCLUDED
 #define GLOBALS_H_INCLUDED
 
+/*
+    Global variables
+*/
+
 extern bool _quit;
 
 extern bool _leftClick;
@@ -24,15 +28,6 @@ extern SDL_Event _e;
 
 extern SDL_Rect _camera;
 extern int _cameraX; extern int _cameraY; extern int _cameraNewX; extern int _cameraNewY; extern int _cameraVelX; extern int _cameraVelY;
-
-extern SDL_Rect _Projectile;
-extern int _projectileVelX;
-extern int _projectileVelY;
-extern int _projectileSpeed;
-extern int _projectileFrames;
-extern int _projectileLocX;
-extern int _projectileLocY;
-extern bool _shoot;
 
 
 #endif // GLOBALS_H_INCLUDED
